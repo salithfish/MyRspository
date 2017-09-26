374e44d84a875889576a88f3f4af278dabd52294
Zhen Yu <18790258213@163.com>
2017-09-26 17:28:59 +0800

    beta

--- src/sum.ts
+++ src/sum.ts
@@ -1,4 +1,24 @@
-function add(x,y){
+//普通函数
+function sum(x,y){
     return x+y;
 }
-console.log(add(5,3));
+
+//Function函数
+let sum1 = new Function("x", "y", "return x+y;");
+document.write(sum1(3,2));
+
+//添加类型
+function sum2(x:number,y:number):number{
+    return x+y;
+}
+
+// 匿名函数
+let  sum3  = function(x,y){return x+y};
+
+//自执行的匿名函数
+let sum4 = (function (x, y) { return x + y; }(3,2));
+document.write(String(sum4));
+
+//箭头函数
+let sum5 = (x, y) => x + y
+document.write(sum5(3, 2));
